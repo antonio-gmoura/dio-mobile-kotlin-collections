1cc909d5067abc3a0dabba1bba3137152c99744c
Antonio Moura <antonio.gmoura@gmail.com>
2021-08-11 01:50:34 -0400

    created extensions functions and test

--- src/main/kotlin/one/digitalinnovation/collections/FuncoesEstendidas.kt
+++ src/main/kotlin/one/digitalinnovation/collections/FuncoesEstendidas.kt
@@ -6,6 +6,6 @@ fun Array<BigDecimal>.somatoria() = this.reduce{
     acc, valor -> acc + valor
 }
 
-fun Array<BigDecimal>.media() =
+fun Array<BigDecimal>.media(): BigDecimal =
     if (this.isEmpty()) BigDecimal.ZERO
     else this.somatoria() / this.size.toBigDecimal()
