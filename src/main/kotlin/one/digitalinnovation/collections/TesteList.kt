2bf39f07575af791b4e0519b127ab2ed039508fa
Antonio Moura <antonio.gmoura@gmail.com>
2021-08-10 23:19:16 -0400

    updated test list, extract employee to separated file and created test set

--- src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -23,15 +23,3 @@ fun main() {
         .groupBy { it.tipoContratacao }
         .forEach { println(it) }
 }
-
-data class Funcionarios(
-    val nome: String,
-    val salario: Double,
-    val tipoContratacao: String
-) {
-    override fun toString(): String =
-        """
-            Nome:    $nome
-            Salário: $salario
-        """.trimIndent()
-}
---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteSet.kt
@@ -0,0 +1,23 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionarios("João", 2000.0, "CLT")
+    val pedro = Funcionarios("Pedro", 1500.0, "PJ")
+    val maria = Funcionarios("Maria", 4000.0, "CLT")
+
+    val funcionarios1 = listOf(joao, pedro)
+    val funcionarios2 = listOf(maria)
+
+    println(TRACOS)
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach{println(it)}
+
+    println(TRACOS)
+    val funcionarios3 = setOf(joao, pedro, maria)
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach{println(it)}
+
+    println(TRACOS)
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach{println(it)}
+}
